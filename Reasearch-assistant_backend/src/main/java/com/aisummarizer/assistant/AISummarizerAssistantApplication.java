6b4f8d57a1ba68fe7dd39589f8ed4e8fb96d9d3b
prashantrana7721 <prashantrana7721@gmail.com>
2025-08-24 19:26:48 +0530

    Update / Fix

---
+++ Reasearch-assistant_backend/src/main/java/com/aisummarizer/assistant/AISummarizerAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.aisummarizer.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AISummarizerAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AISummarizerAssistantApplication.class, args);
+	}
+
+}
---
+++ Reasearch-assistant_backend/src/main/java/com/aisummarizer/assistant/ResearchRequest.java
@@ -0,0 +1,9 @@
+package com.aisummarizer.assistant;
+
+import lombok.Data;
+
+@Data
+public class ResearchRequest {
+    private String content;
+    private String operation;
+}
